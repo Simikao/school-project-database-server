18a91b9ef07b3ff02820d11164975d1c15bff020
Simikao <s.wierzejski.420@studms.ug.edu.pl>
2024-01-20 00:17:03 +0100

    add server shutdown + simple CRUD
    Added simple CRUD, handled server shutdown and added authentication for database admin

--- go.mod
+++ go.mod
@@ -4,15 +4,18 @@ go 1.21.6
 
 require (
 	github.com/gin-gonic/gin v1.9.1
-	github.com/google/uuid v1.5.0
 	go.mongodb.org/mongo-driver v1.13.1
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/charmbracelet/log v0.3.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
@@ -22,11 +25,16 @@ require (
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -35,9 +43,10 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
-	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -2,72 +2,67 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"net/mail"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"github.com/charmbracelet/log"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type User struct {
-	Email    string    `json:"email"`
-	Password string    `json:"password"`
-	Name     string    `json:"name"`
-	Uuid     uuid.UUID `json:"id"`
+	Email    string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+	Name     string `json:"name" bson:"name"`
 }
 
 type Post struct {
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	Author  User   `json:"author"`
+	Title   string `json:"title" bson:"title"`
+	Content string `json:"content" bson:"content"`
+	Author  User   `json:"author" bson:"author"`
 }
 
-var uri = "mongodb://root:example@localhost:27017/?timeoutMS=1000"
+var (
+	uri  = "mongodb://localhost:27017"
+	auth = options.Credential{
+		Username: "root",
+		Password: "example",
+	}
+)
 
 func main() {
+	log.Info("Hello world")
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri).SetAuth(auth))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Set up failed: ", err)
+	}
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Connection failed: ", err)
 	}
 
 	redditos := client.Database("redditos")
 	ex := redditos.Collection("example")
 
-	id, err := ex.InsertOne(ctx, bson.M{"foo": "bar", "dorrito": "dust"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := ex.FindOne(ctx, bson.M{"_id": id.InsertedID})
-	if err := result.Err(); err != nil {
-		log.Fatal("50", err)
-	}
-
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		r := struct {
-			Name  string `bson:"foo" json:"fiddle"`
-			Name2 string `bson:"dorrito" json:"crisps"`
-		}{}
-		err := result.Decode(&r)
-		if err != nil {
-			c.AbortWithStatus(500)
-			return
-		}
-		c.JSON(200, r)
+		c.String(200, "hello World")
 	})
 	r.POST("/", func(c *gin.Context) {
 		u := User{
 			Email:    "ala@niemako.ta",
 			Name:     "Adam",
 			Password: "hashysz",
-			Uuid:     uuid.New(),
 		}
 		post := Post{
 			Title:   "Bigsmall World",
@@ -76,5 +71,71 @@ func main() {
 		}
 		c.JSON(200, post)
 	})
-	r.Run()
+	r.POST("/add", func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		defer cancel()
+		newUser := User{
+			Email:    "ala@niemako.ta",
+			Name:     "Adam",
+			Password: "hashysz",
+		}
+		_, err := mail.ParseAddress(newUser.Email)
+		if err != nil {
+			c.String(200, "Wrong email address")
+		}
+		id, err := ex.InsertOne(ctx, newUser)
+		if err != nil {
+			c.String(500, err.Error())
+			return
+		}
+		c.String(200, id.InsertedID.(primitive.ObjectID).Hex())
+		var result User
+		err = ex.FindOne(ctx, bson.M{"_id": id.InsertedID}).Decode(&result)
+		if err != nil {
+			c.String(500, "Something went wrong")
+			return
+		}
+		c.JSON(200, result)
+
+	})
+	r.GET("/find", func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+		defer cancel()
+
+		cur, err := ex.Find(ctx, bson.D{})
+		if err != nil {
+			c.String(500, err.Error())
+		}
+		var results []User
+		for cur.Next(ctx) {
+			var elem User
+			err := cur.Decode(&elem)
+			if err != nil {
+				c.String(500, err.Error())
+			}
+			results = append(results, elem)
+
+		}
+		c.JSON(200, results)
+
+	})
+
+	go r.Run()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	quitCtx, quitCancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer quitCancel()
+	err = ex.Drop(quitCtx)
+	fmt.Println("Dropping database")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Closing connection")
+	err = ex.Database().Client().Disconnect(quitCtx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 }
