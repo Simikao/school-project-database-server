5bcbc35cdc7ce3c695b4e70cd1fa6793f0e63b6f
Simikao <s.wierzejski.420@studms.ug.edu.pl>
2024-01-19 20:22:52 +0100

    connect and operate mongo
    succesfully connected to mongo through docker and added and accessed data inside

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"log"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type User struct {
@@ -18,11 +25,42 @@ type Post struct {
 	Author  User   `json:"author"`
 }
 
+var uri = "mongodb://root:example@localhost:27017/?timeoutMS=1000"
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redditos := client.Database("redditos")
+	ex := redditos.Collection("example")
+
+	id, err := ex.InsertOne(ctx, bson.M{"foo": "bar", "dorrito": "dust"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := ex.FindOne(ctx, bson.M{"_id": id.InsertedID})
+	if err := result.Err(); err != nil {
+		log.Fatal("50", err)
+	}
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
+		r := struct {
+			Name  string `bson:"foo" json:"fiddle"`
+			Name2 string `bson:"dorrito" json:"crisps"`
+		}{}
+		err := result.Decode(&r)
+		if err != nil {
+			c.AbortWithStatus(500)
+			return
+		}
+		c.JSON(200, r)
 	})
 	r.POST("/", func(c *gin.Context) {
 		u := User{
